649e6c3c672bbde3e4b5aa504347c60d90705c92
Iyanuoluwa fanoro <iycodes@outlook.com>
2023-12-31 21:43:28 +0100

    sd

---
+++ js/intercom.js
@@ -0,0 +1,1 @@
+!function(){var t=window,e=t.Intercom;if("function"==typeof e)e("reattach_activator"),e("update",t.intercomSettings);else{var n,c=document,i=function(){i.c(arguments)};i.q=[],i.c=function(t){i.q.push(t)},t.Intercom=i,(n=c.createElement("script")).type="text/javascript",n.async=!0,n.src="https://widget.intercom.io/widget/xi5kvfyn",document.body.appendChild(n)}}();
